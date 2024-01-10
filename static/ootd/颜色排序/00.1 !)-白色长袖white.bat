0e06ce0d75ac930a1c408f3891ccf773ed819abe
eezTool <theashes@qq.com>
2024-01-11 05:57:56 +0800

    更新ootd结构

---
+++ static/ootd/颜色排序/00.1 !)-白色长袖white.bat
@@ -0,0 +1,15 @@
+chcp 65001
+set pre=00.1
+@echo off 
+:main 
+if not "%~1"=="" ( 
+  if exist "%~1" ( 
+    move "%~nx1" "%~dp0\%pre% %~nx1" 
+  ) else ( 
+     echo "%~nx1" not exist 
+  ) 
+  shift /1 
+  goto :main 
+) 
+:: 
+::pause 
